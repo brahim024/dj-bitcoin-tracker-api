44c72c5b10937d438f868769320c40eea51c3ec2
brahim024 <ifninos168@gmail.com>
2020-12-22 13:37:56 +0100

    test api json response

--- prices/models.py
+++ prices/models.py
@@ -5,7 +5,7 @@ class Prices(models.Model):
     name = models.CharField(max_length=100)
     image = models.URLField()
     price = models.CharField(max_length=100)
-    cap_rank = modlels.CharField(max_length=100)
+    cap_rank = models.CharField(max_length=100)
     market_cap = models.CharField(max_length=100)
     def __str__(self):
         return self.name
--- prices/views.py
+++ prices/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import requests
 # Create your views here.
+def price(request):
+    url='https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    data=requests.get(url).json()
+    return HttpResponse(data)
+
