3e503447483853895b6235a004691d1655ec8ea1
brahim024 <ifninos168@gmail.com>
2020-12-22 13:17:14 +0100

    add prices models

--- prices/models.py
+++ prices/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Prices(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.URLField()
+    price = models.CharField(max_length=100)
+    cap_rank = modlels.CharField(max_length=100)
+    market_cap = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
