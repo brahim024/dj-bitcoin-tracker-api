8c92a795de7f01fb5a1706dd58d94bc7c0ac6fcd
brahim024 <ifninos168@gmail.com>
2020-12-22 21:40:51 +0100

    remove periodic tasks

--- prices/tasks.py
+++ prices/tasks.py
@@ -1,11 +1,13 @@
 from celery import shared_task
 from .models import Prices
-
-
+from celery.decorators import periodic_task
+#from celery.tasks.schedules import crontab
 @shared_task
-def create_price_project(name,image,price,cap_rank,market_cap):
+def create_price_items(name,image,price,cap_rank,market_cap):
     Price.objects.create(name=name)
     Price.objects.create(image=image)
     Price.objects.create(price=price)
     Price.objects.create(market_cap=market_cap)
-    
+#@periodic_task(run_every=(crontab(minute='*/1')))
+#def run_create_items():
+#    create_price_items.delay(name='new2020')
