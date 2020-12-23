470a08a31183444286be27ef4a53e26267439de2
brahim024 <ifninos168@gmail.com>
2020-12-23 13:25:52 +0100

    add midelware

--- crypto/__init__.py
+++ crypto/__init__.py
@@ -1,3 +1,3 @@
-from .celery import app as prices
+from .celery import app as celery_app
 
-__all__ = ('prices',)
+__all__ = ('celery_app',)
--- crypto/settings.py
+++ crypto/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -44,6 +44,7 @@ CELERY_BROKER='redis://localhost:6379'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
